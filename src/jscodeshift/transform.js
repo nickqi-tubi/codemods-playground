e5ee13977df25c3380c78df22d5b89961b579644
Nick Qi <nickqi@tubi.tv>
2022-10-19 14:03:35 +0800

    feat: transform enzyme import to RTL import

--- src/jscodeshift/transform.js
+++ src/jscodeshift/transform.js
@@ -1,7 +1,21 @@
-module.exports = (fileInfo, api, options) => {
-  console.log(fileInfo);
-  // transform `fileInfo.source` here
-  // ...
-  // return changed source
-  return fileInfo.source;
+export const parser = 'tsx';
+
+module.exports = (file, api) => {
+  const j = api.jscodeshift;
+
+  return j(file.source)
+    .find(j.ImportDeclaration, {
+      specifiers: [
+        {
+          imported: {
+            name: 'mount',
+          },
+        },
+      ],
+      source: {
+        value: 'enzyme',
+      },
+    })
+    .replaceWith(`import { render } from '@testing-library/react';`)
+    .toSource();
 };
